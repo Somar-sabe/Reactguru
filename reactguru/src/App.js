e4342510f432d75dddb36755e45d9543fe0701b8
Somar-sabe <119017905+Somar-sabe@users.noreply.github.com>
2023-11-29 18:19:33 +0400

    Update App.js

--- reactguru/src/App.js
+++ reactguru/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import login from './pages/login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
 import Registration from './pages/register';
 import ExpenseListing from './pages/expenseListing';
 import PasswordReset from './pages/resetpassword';
@@ -9,13 +9,13 @@ import ExpenseManagement from './pages/expansemangment';
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={login} />
-        <Route path="/expanse-management" component={ExpenseManagement} />
-        <Route path="/expenselisting" component={ExpenseListing} />
-        <Route path="/register" component={Registration} />
-        <Route path="/passwordreset" component={PasswordReset} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/expanse-management" element={<ExpenseManagement />} />
+        <Route path="/expenselisting" element={<ExpenseListing />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/passwordreset" element={<PasswordReset />} />
+      </Routes>
     </Router>
   );
 };
